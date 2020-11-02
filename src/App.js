19ed90bd8a725cef65d5f4ba787cae8d6404a320
Dilenyan <TDilenyan@luxoft.com>
2020-11-02 17:54:30 +0200

    t

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Angular
+          Learn React and be happy
         </a>
       </header>
     </div>

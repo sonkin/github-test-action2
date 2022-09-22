fd09d433659b72662e590f04ac218f02174acaf4
Vladimir Sonkin <vladson@ya.ru>
2022-09-22 09:20:11 +0300

    correct update now

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learning react!!
+          Learn react!!
         </a>
       </header>
     </div>
